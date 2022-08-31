bdce287f2a1b7631067fa8769816e8cfcdbf2695
Nosi <nosichefane@gmail.com>
2022-08-31 20:11:20 +0200

    final landing page

--- car_theft_alert_App/src/components/Home.tsx
+++ car_theft_alert_App/src/components/Home.tsx
@@ -17,9 +17,10 @@ function Home() {
           <Card style={{width:'50rem', height: '30rem', marginLeft:'280px', marginTop:'80px'}}>
       <Card.Body>
         <Card.Title style={{fontSize: '2rem' }}>Welcome to Car Theft Alert App</Card.Title>
+        <img src={alert} alt="Logo" style={{height: '15rem', width:'20rem'}} />
         <Card.Subtitle className="mb-2 text-muted">The Right Platform For Searching For Your Stolen Vehicle! Let Us Help You!</Card.Subtitle>
         <Card.Text>
-         <img src={alert} alt="Logo" style={{height: '15rem', width:'20rem'}} />
+         
         </Card.Text>
       
         <Button variant="primary" onClick={navigateToAboutus}> More About Car Theft Alert App</Button>
