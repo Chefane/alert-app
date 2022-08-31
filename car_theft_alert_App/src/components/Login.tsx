eddbfe462ff16b8a8b63bbbdb4e259f8b89c5433
Leeto Tenki <boshoabotsoere7@gmail.com>
2022-08-31 21:15:10 +0200

    added users login form

--- car_theft_alert_App/src/components/Login.tsx
+++ car_theft_alert_App/src/components/Login.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import Button from 'react-bootstrap/Button';
+import '../App.css';
+import Form from 'react-bootstrap/Form';
 
 
+//Login component that will allow users to login, users have to use Email and password to login
 function Login() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div>
+        <h2>Login</h2>
+        <Form>
+         <img src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="profile-img"
+        className="profile-img-card"/>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+             .
+            </Form.Text>
+          </Form.Group>
+    
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter Password" required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+        </div>
+    )
 }
-
 export default Login;
