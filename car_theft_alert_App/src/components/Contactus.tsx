df3b777537b548c1a92b864e864d423163eb81a2
Leeto Tenki <boshoabotsoere7@gmail.com>
2022-08-31 21:08:45 +0200

    added contact us details

--- car_theft_alert_App/src/components/Contactus.tsx
+++ car_theft_alert_App/src/components/Contactus.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import  React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import phone from './images/contact.webp'; 
 
-
+//Below function is going to be used to display our contact details
 function Contactus() {
   return (
-    <div className="App">
-      
+    <div className="contact">
+      <img src={phone}  height="10%" width="30%" alt="Our Logo" />
+      <p>You can cntact us here:</p>
+      <ul>
+        <li>+266 56787521</li>
+        <li>+266 59146533</li>
+      </ul>
+      <p>For formal communication, Emails us here: <br />code.technology@gmail.com</p>
     </div>
   );
 }
-
 export default Contactus;
+
